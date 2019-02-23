12ee4c2eb1972bca0f128d8d950f3f6b8870f1d0
Jue <me@jue.yt>
2019-02-23 16:10:08 +0100

    Renamed Realm to Hood

--- pki/createPeer.sh
+++ pki/createPeer.sh
@@ -5,7 +5,7 @@ err() {
 	exit
 }
 
-[ $# -le 1 ] && err "Usage: createPeer [realm] [name]"
+[ $# -le 1 ] && err "Usage: createPeer [hood] [name]"
 
 # Prepare some vars
 [ ! -e config.sh ] && err "The config is missing! Please create \"config.sh\""
@@ -13,16 +13,16 @@ err() {
 [ -z $COUNTRY ] && err "Variable COUNTRY is not set in config.sh"
 [ -z $STATE ] && err "Variable STATE is not set in config.sh"
 [ -z $LOCALITY ] && err "Variable LOCALITY is not set in config.sh"
-REALM=$1
+HOOD=$1
 PEER=$2
 
-# Check if realm stuff is present
-[ ! -e $REALM.crt ] && err "Realm certificate is missing"
-[ ! -e $REALM.key ] && err "Realm key is missing"
-[ -e $REALM.$PEER.key -o -e $REALM.$PEER.key ] && err "Peer is already existing"
+# Check if hood stuff is present
+[ ! -e $HOOD.crt ] && err "Hood certificate is missing"
+[ ! -e $HOOD.key ] && err "Hood key is missing"
+[ -e $HOOD.$PEER.key -o -e $HOOD.$PEER.key ] && err "Peer is already existing"
 
 # Create key, csr and finally sign the csr
-openssl genrsa -out $REALM.$PEER.key 4096 || exit
-openssl req -new -key $REALM.$PEER.key -out $REALM.$PEER.csr  -subj "/C=$COUNTRY/ST=$STATE/L=$LOCALITY/O=$REALM/OU=Peer/CN=$PEER" || exit
-openssl x509 -req -days 1095 -in $REALM.$PEER.csr -CA $REALM.crt -CAkey $REALM.key -out $REALM.$PEER.crt -set_serial $(date +%s) || exit
-rm $REALM.$PEER.csr
+openssl genrsa -out $HOOD.$PEER.key 4096 || exit
+openssl req -new -key $HOOD.$PEER.key -out $HOOD.$PEER.csr  -subj "/C=$COUNTRY/ST=$STATE/L=$LOCALITY/O=$HOOD/OU=Peer/CN=$PEER" || exit
+openssl x509 -req -days 1095 -in $HOOD.$PEER.csr -CA $HOOD.crt -CAkey $HOOD.key -out $HOOD.$PEER.crt -set_serial $(date +%s) || exit
+rm $HOOD.$PEER.csr
